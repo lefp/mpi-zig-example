8dac10b5ebf04cfe9211805ce786d9215b20a689
Peter Lef <lefp@clarkson.edu>
2023-05-09 08:47:32 -0400

    The functions were supposed to be inline

--- src/zmpi-cwrapper.c
+++ src/zmpi-cwrapper.c
@@ -1,29 +1,29 @@
 #include <mpi.h>
 
-int init(int* argc, char*** argv) {
+extern inline int init(int* argc, char*** argv) {
   return MPI_Init(argc, argv);
 }
 
-int finalize() {
+extern inline int finalize() {
   return MPI_Finalize();
 }
 
-MPI_Comm commWorld() {
+extern inline MPI_Comm commWorld() {
   return MPI_COMM_WORLD;
 }
 
-int commSize(MPI_Comm comm, int* result) {
+extern inline int commSize(MPI_Comm comm, int* result) {
   return MPI_Comm_size(comm, result);
 }
 
-int commRank(MPI_Comm comm, int* result) {
+extern inline int commRank(MPI_Comm comm, int* result) {
   return MPI_Comm_rank(comm, result);
 }
 
-int maxProcessorName() {
+extern inline int maxProcessorName() {
   return MPI_MAX_PROCESSOR_NAME;
 }
 
-int getProcessorName(char* name, int* result_len) {
+extern inline int getProcessorName(char* name, int* result_len) {
   return MPI_Get_processor_name(name, result_len);
 }
